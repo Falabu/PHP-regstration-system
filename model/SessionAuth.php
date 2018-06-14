0c808fa054bf64b0dfcc230a87e1442ff5cd2190
Falabu <david.kurucz@gmail.com>
2018-06-14 14:31:48 +0200

    Add files via upload

--- model/SessionAuth.php
+++ model/SessionAuth.php
@@ -1,9 +1,37 @@
 <?php
 /**
  * Class SessionAuth
+ * @uses SessionAuthDataManager
  *
- * Creates, manages the authentication across the whole application, it uses the SessionAuthDataManager for
- * database querys.
+ * Creates, deletes, manages the authentication, it uses the SessionAuthDataManager for
+ * database query's.
+ *
+ * Copyright (C) 2018 by Kurucz Dávid
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author Kurucz Dávid
+ *
+ *
+ * @param $sessionHash string
+ * @param $uId int
+ * @param $sessionDb SessionAuthDataManager
  */
 
 class SessionAuth
@@ -42,7 +70,7 @@ class SessionAuth
     }
 
     /**
-     * Delete the $_SESSION and delete the session in the database, logging out the user.
+     * Delete the $_SESSION global and delete the session in the database, logging out the user.
      *
      * @return bool
      */
