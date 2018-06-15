12efd8e7fc2656cb8de31ef6643779974da9f779
Falabu <david.kurucz@gmail.com>
2018-06-15 14:25:51 +0200

    Add files via upload

--- model/Code.php
+++ model/Code.php
@@ -1,36 +1,9 @@
 <?php
 /**
- * Ide beírok valamit elehetasdadsad
- * Ide is beírok valamit
- *
- * trtrtzrtrt
- * sdfsf
- * sdfsdfsfsfsfgffghfg
- * dfgddfgdfgddgdfg
- *
- * Copyright (C) 2018 by Kurucz Dávid
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @author Kurucz Dávid
- *
- *
+ * Created by PhpStorm.
+ * User: DaweTheDrummer
+ * Date: 2018. 06. 13.
+ * Time: 14:57
  */
 
 class Code
@@ -81,10 +54,28 @@ class Code
 
     public function getCodes()
     {
-        return $this->codeDb->get($this->uId);
+        return $this->formatCodes($this->codeDb->get($this->uId));
     }
 
-    public function uploadCodes()
+    private function formatCodes($codeArray)
+    {
+        $tmpCodeArray = array();
+
+        for ($j = 0; $j < count($codeArray); $j++) {
+            $tmpCodeArray[$j] = "";
+            for ($i = 0; $i < strlen($codeArray[$j]); $i++) {
+                if ($i % 4 == 0 && $i != 0) {
+                    $tmpCodeArray[$j] .= "-";
+                }
+                $tmpCodeArray[$j] .= $codeArray[$j][$i];
+            }
+        }
+
+        return $tmpCodeArray;
+    }
+
+
+    public function uploadCode()
     {
         if ($this->lenghtCheck()) {
             if ($this->codeChecker()) {
@@ -95,7 +86,7 @@ class Code
                 errorLogger::writeUserMessages("A kód csak betűt és számot tartalmazhat és ékezetes karaktert nem");
                 return false;
             }
-        }else{
+        } else {
             errorLogger::writeUserMessages("A kódnak 24 jegyűnek kell lennie");
             return false;
         }
