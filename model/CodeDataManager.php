21ddac7dbcda60b0dff490157e53a035fad7dd00
Falabu <david.kurucz@gmail.com>
2018-06-15 14:42:16 +0200

    Created the comments

--- model/CodeDataManager.php
+++ model/CodeDataManager.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * A helper class for Code object, it conatins the database querys
  *
  *
  * Copyright (C) 2018 by Kurucz Dávid
@@ -33,16 +33,35 @@ class CodeDataManager
 {
     private $db;
 
+    /**
+     * Creating the connection
+     *
+     * CodeDataManager constructor.
+     *
+     */
     public function __construct()
     {
         $this->db = dbConnect::dbCon();
     }
 
+    /**
+     * Upload to database with a specific user ID
+     *
+     * @param $uId int User Id
+     * @param $code string Code
+     */
     public function upload($uId, $code){
         $sql = $this->db->prepare("INSERT INTO codes(code, uId) VALUES (?,?)");
         $sql->execute([$code,$uId]);
     }
 
+    /**
+     * Gets the codes for the specific User ID
+     *
+     * @param $uId int User Id
+     * @return array This array contains the codes
+     *
+     */
     public function get($uId){
         $codes = array();
         $sql = $this->db->prepare("SELECT code FROM codes WHERE uId= ? ");
